c0c9d800e87238ba863d08b66dccd21bf73e6729
tentiap <inooyabu@gmail.com>
2021-06-22 03:17:51 +0700

    Ready Go!

--- SalfokApp/ViewController.swift
+++ SalfokApp/ViewController.swift
@@ -9,18 +9,4 @@ import UIKit
 
 class ViewController: UIViewController {
     
-//    private let swipeableView: UIView = {
-//        let view = UIView(frame: <#T##CGRect#>(origin: .zero,
-//                                        size: CGSize(width: 200.0, height: 200.0)))
-//
-//
-//    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
 }
-
