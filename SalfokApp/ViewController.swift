27dcc0113e928a2fdf97c38440ab028952ddaa94
lilylub <lilyartha@gmail.com>
2021-06-22 20:14:40 +0700

    tampilan view controller storyboard 4,5

--- SalfokApp/ViewController.swift
+++ SalfokApp/ViewController.swift
@@ -9,9 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var infoHowToPlayButton: UIButton!
+    @IBOutlet weak var closeButton: UIButton!
+    @IBOutlet weak var scoringLabel: UILabel!
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var charLabel: UILabel!
+    @IBOutlet weak var sameButton: UIButton!
+    @IBOutlet weak var differentButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        sameButton.layer.cornerRadius = 15
+        differentButton.layer.cornerRadius = 15
     }
 
 
