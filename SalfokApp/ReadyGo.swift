af08b4b7f93231d3f674da9b293b47bb9b6a304f
Evita <veronikaevita@gmail.com>
2021-06-24 18:08:42 +0700

    menambah fungsi screen Ready G0

--- SalfokApp/ReadyGo.swift
+++ SalfokApp/ReadyGo.swift
@@ -11,39 +11,46 @@ class ReadyGo: UIViewController {
 
     @IBOutlet weak var readyGoLabel: UILabel!
     
-    //    var counter = 3.0
-//    var timer = Timer()
-//
-//    timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(runTimer), userInfo: nil, repeats: true)
+    @IBOutlet var lblTimer: UILabel!
+    
+    var counter = 3
+    var timer = Timer()
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        timer.invalidate()
+        timer = Timer.scheduledTimer(timeInterval: 1.5, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
 
         // Do any additional setup after loading the view.
     }
-    //    @objc func runTimer(){
-//        counter -=  0.1
-//        print(counter)
-//
-//        let flooredCounter = Int(floor(counter))
-//        var minute = flooredCounter / 60
-//        var minuteString = "0\(minute)"
-//
-//        let second = flooredCounter % 60
-//        var secondString = "\(second)"
-//        if second < 10{
-//            secondString = "0\(second)"
-//        }
-//
-////        let decisecond = String(format: "%.1f", counter).components(separatedBy: ".").last!
-//
-//        timerLabel.text = "\(minuteString):\(secondString)"
-//
-//        if minute == 0 && second == 0{
-//            timer.invalidate()
-//            counter = 60.0
-//
-//            let vc = storyboard?.instantiateViewController(identifier: "gameScreen") as! GameScreenViewController
+    
+    
+    @objc func timerAction() {
+        counter -= 1
+        lblTimer.text = "\(counter)"
+        
+        if counter <= 0{
+            timer.invalidate()
+            lblTimer.text = " "
+            
+            let vc = storyboard?.instantiateViewController(identifier: "gameScreen") as! GameScreenViewController
+            vc.modalPresentationStyle = .fullScreen
+            self.present(vc, animated: true)
+            
+        }
+        
+        if counter <= 1 {
+            readyGoLabel.text = "GO !"
+           
+        }
+        
+        
+    }
+    
+
+//         let vc = storyboard?.instantiateViewController(identifier: "gameScreen") as! GameScreenViewController
 //            vc.modalPresentationStyle = .fullScreen
 ////            present(vc,animated: true)
 //
