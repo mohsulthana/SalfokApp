f541f606d33d79309cd74bd1059792d14359b375
Mohammad Sulthan <muhammadsultana@outlook.com>
2021-06-24 11:02:23 +0700

    terakhir nih gaes: data dibawa ke segue

--- SalfokApp/SummaryViewController.swift
+++ SalfokApp/SummaryViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 class SummaryViewController: UIViewController {
 
     @IBOutlet weak var playAgainButton: UIButton!
-    
+    @IBOutlet weak var scoreView: UILabel!
+    @IBOutlet weak var correctView: UILabel!
+    @IBOutlet weak var incorrectView: UILabel!
+    var takeScore = Int()
+    var takeCorrect = Int()
+    var takeInccorect = Int()
     override func viewDidLoad() {
         super.viewDidLoad()
 
         playAgainButton.layer.cornerRadius = 15
+        scoreView.text = String(takeScore)
+        correctView.text  = String(takeCorrect)
+        incorrectView.text = String(takeInccorect)
     }
     
 
